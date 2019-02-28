145223f1f65525f052a4bb88a1fcd03f0aed49ab
Leonardo Cserny <leonardo_cserny@netex.ro>
2019-02-28 16:33:05 +0200

    adds levenshteinDistance

--- utils.go
+++ utils.go
@@ -81,3 +81,34 @@ func GetRealPath(path string) (string, os.FileInfo) {
 
 	return path, fileInfo
 }
+
+func LevenshteinDistance(source, target string) int {
+	d := make([][]int, len(source)+1)
+	for i := range d {
+		d[i] = make([]int, len(target)+1)
+	}
+	for i := range d {
+		d[i][0] = i
+	}
+	for j := range d[0] {
+		d[0][j] = j
+	}
+	for j := 1; j <= len(target); j++ {
+		for i := 1; i <= len(source); i++ {
+			if source[i-1] == target[j-1] {
+				d[i][j] = d[i-1][j-1]
+			} else {
+				min := d[i-1][j]
+				if d[i][j-1] < min {
+					min = d[i][j-1]
+				}
+				if d[i-1][j-1] < min {
+					min = d[i-1][j-1]
+				}
+				d[i][j] = min + 1
+			}
+		}
+
+	}
+	return d[len(source)][len(target)]
+}
